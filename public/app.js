2a83bfaf2e52faf55d933f4cb21257ce47ffdc2d
Ashish S <s.cricket.ashish@gmail.com>
2019-02-03 12:59:10 +0530

    data-fetch

--- public/app.js
+++ public/app.js
@@ -1,11 +1,29 @@
-document.addEventListener('DOMContentLoaded', e => {
-  const app = firebase.app();
-  console.log(app);
+const dataset = document.querySelector('.dataset');
+const dataheads = document.querySelectorAll('.data-head span');
 
+// create element
+function renderData(d) {
+  let div = document.createElement('div');
+  let data = d.data();
+  let dataheadCount = 0;
+  console.log(data, dataheads);
+
+  for (const key in data) {
+    if (data.hasOwnProperty(key)) {
+      let span = document.createElement('span');
+      span.textContent = data[dataheads[dataheadCount++].textContent];
+      div.appendChild(span);
+      console.log(key);
+    }
+  }
+  dataset.appendChild(div);
+}
+
+document.addEventListener('DOMContentLoaded', e => {
   const db = firebase.firestore();
   const automobiles = db.collection('automobiles');
 
   automobiles.get().then(data => {
-    data.docs.forEach(doc => console.log(doc.data()));
+    data.docs.forEach(doc => renderData(doc));
   });
 });
